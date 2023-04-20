ca8faef02e0501be674a24575a659c9f373c2a76
Kelvin Koome <kelvinkoomeh@gmail.com>
2023-04-20 13:36:04 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,9 +9,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-
 	va_start(list, n);
-
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
@@ -21,8 +19,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s%d", separator, va_arg(list, int));
 	}
-
 	va_end(list);
-
 	printf("\n");
 }
