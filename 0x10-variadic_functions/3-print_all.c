15b31670b540268cf73e092ef6d6c1f2466e6742
Kelvin Koome <kelvinkoomeh@gmail.com>
2023-04-20 14:10:09 -0700

    Prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * print_all - prints anything
- * @format list of data types of arguments passed to the function
+ * @format: list of data types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
